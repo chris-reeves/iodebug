3b2535fa85d0d30fd6feafc99e3f10b1aa6b4096
Chris Reeves <chris.reeves@iname.com>
2021-04-04 11:18:00 +0100

    Dump hex version of data read when using DebugReader

--- reader.go
+++ reader.go
@@ -1,7 +1,11 @@
 package iodebug
 
 import (
+	"bufio"
+	"encoding/hex"
 	"io"
+	"log"
+	"strings"
 )
 
 type DebugReader struct {
@@ -16,5 +20,12 @@ func NewDebugReader(r io.Reader) io.Reader {
 
 func (r *DebugReader) Read(p []byte) (n int, err error) {
 	n, err = r.subReader.Read(p)
+
+	// Hex dump of data to logger, line by line
+	scanner := bufio.NewScanner(strings.NewReader(hex.Dump(p[:n])))
+	for scanner.Scan() {
+		log.Print(scanner.Text())
+	}
+
 	return n, err
 }
--- reader_test.go
+++ reader_test.go
@@ -1,11 +1,16 @@
 package iodebug
 
 import (
+	"log"
 	"strings"
 	"testing"
 )
 
 func TestReadReturnsExpectedData(t *testing.T) {
+	// Configure logging to a string
+	var logOutput strings.Builder
+	log.SetOutput(&logOutput)
+
 	// Set up a sub-reader
 	str := "This is a test message!"
 	strReader := strings.NewReader(str)
@@ -24,5 +29,19 @@ func TestReadReturnsExpectedData(t *testing.T) {
 	}
 	if string(buff[:n]) != str {
 		t.Errorf("expected: buff[:n] === %q, got: %q", str, string(buff[:n]))
+	}
+
+	// Test debug output - we must strip the log output prefix first as this
+	// default to date so will vary for every test
+	prefixLength := 20
+	expectedOutput := [2]string{
+		"00000000  54 68 69 73 20 69 73 20  61 20 74 65 73 74 20 6d  |This is a test m|",
+		"00000010  65 73 73 61 67 65 21                              |essage!|",
+	}
+	receivedOutput := strings.Split(logOutput.String(), "\n")
+	for i, _ := range expectedOutput {
+		if receivedOutput[i][prefixLength:] != expectedOutput[i] {
+			t.Errorf("expected: logOutput[%d] === %q, got: %q", i, expectedOutput[i][prefixLength:], receivedOutput[i])
+		}
 	}
 }
