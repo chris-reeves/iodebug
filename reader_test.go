27509db09b4047f2e03af66566f6097b33ea516b
Chris Reeves <chris.reeves@iname.com>
2021-04-04 11:16:59 +0100

    Improve the read tests
    * increase length of string in the test
    * make the tests better adapt to whatever string is used

--- reader_test.go
+++ reader_test.go
@@ -7,17 +7,17 @@ import (
 
 func TestReadReturnsExpectedData(t *testing.T) {
 	// Set up a sub-reader
-	str := "This is a test!"
+	str := "This is a test message!"
 	strReader := strings.NewReader(str)
 
 	// Create a DebugReader and then Read()
 	r := NewDebugReader(strReader)
-	buff := make([]byte, 20)
+	buff := make([]byte, 32)
 	n, err := r.Read(buff)
 
 	// Test data returned by Read()
-	if n != 15 {
-		t.Errorf("expected: n === 15, got: %d", n)
+	if n != len(str) {
+		t.Errorf("expected: n === %d, got: %d", len(str), n)
 	}
 	if err != nil {
 		t.Errorf("expected: err === nil, got: %q", err)
